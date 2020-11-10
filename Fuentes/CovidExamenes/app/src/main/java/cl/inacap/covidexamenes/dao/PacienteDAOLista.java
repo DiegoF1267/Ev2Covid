60f0ff99065cd5508f053039fb1edbd20994d651
diegof <diegolimon123@gmail.com>
2020-11-10 19:49:12 -0300

    realize el dao lista

--- Fuentes/CovidExamenes/app/src/main/java/cl/inacap/covidexamenes/dao/PacienteDAOLista.java
+++ Fuentes/CovidExamenes/app/src/main/java/cl/inacap/covidexamenes/dao/PacienteDAOLista.java
@@ -21,6 +21,17 @@ public class PacienteDAOLista implements PacientesDAO {
         p.setTemperatura(39);
         p.setTos("no");
         pacientes.add(p);
+        p = new Pacientes();
+        p.setNombre("diegolimon");
+        p.setApellido("Fernandez");
+        p.setArea("otro");
+        p.setFecha("20/05/2020");
+        p.setPresion(80);
+        p.setRut("20321269-0");
+        p.setsCovid("si");
+        p.setTemperatura(39);
+        p.setTos("no");
+        pacientes.add(p);
     }
 
     public static PacienteDAOLista getInstance() {
