dc63610a8049920191c693718c33c5614a6bbe31
Jeyzon Godoy <jeyzongodoy@live.cl>
2020-11-10 22:06:45 -0300

    Se genero el Login en el activity_main y se genero un segundo activity que se  usara para el listview

--- Fuentes/CovidExamenes/app/src/main/java/cl/inacap/covidexamenes/MainActivity.java
+++ Fuentes/CovidExamenes/app/src/main/java/cl/inacap/covidexamenes/MainActivity.java
@@ -4,15 +4,27 @@ package cl.inacap.covidexamenes;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button segundoBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        segundoBtn = findViewById(R.id.segundo_btn);
+        segundoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,SegundoActivity.class);
+                startActivity(i);
+            }
+        });
 
     }
 
